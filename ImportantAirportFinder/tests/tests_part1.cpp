19272c92d913448baf9a933d1ec81e2fb7f2d593
Haoxuan Fan <hfan12@illinois.edu>
2022-11-29 05:14:58 +0000

    Rewrite makefile and delete the edge class

---
+++ ImportantAirportFinder/tests/tests_part1.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include "readFile.h"
+#include "graph.h"
+
+TEST_CASE("testing test case", "[weight=5]") {
+    int a = 1;
+    int b = 1;
+    REQUIRE(a == b);
+}
